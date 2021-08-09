cf7dda5025929c31adf34920f4f22a8f74bfe012
Juntian Zheng <j68zheng@uwaterloo.ca>
2021-08-08 20:16:01 -0400

    Completed the web map

--- map1.py
+++ map1.py
@@ -1,19 +1,65 @@
 import folium
+import pandas
+
+# load data from file using pandas
+data = pandas.read_csv("Webmap_datasources/Volcanoes.txt")
+# store latitude, longitude and elevation into lists
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
 
 # from folium, creating a map based on location and set zooming and tiles
 map = folium.Map(
-    location=[43.773291, -79.412817], zoom_start=10, tiles="Stamen Terrain"
+    location=[41.983208, -114.021853], zoom_start=5, tiles="Stamen Terrain"
 )
 # create a feature group
-fg = folium.FeatureGroup(name="My Map")
+fg_v = folium.FeatureGroup(name="Volcanoes")
 # add objects to the feature group
-for coordinates in [[43.773291, -79.412817], [44, -79]]:
-    fg.add_child(
-        folium.Marker(
-            location=coordinates, popup="Test", icon=folium.Icon(color="green"),
+for lt, ln, el in zip(lat, lon, elev):
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg_v.add_child(
+        folium.CircleMarker(
+            location=[lt, ln],
+            radius=6,
+            popup=folium.Popup(iframe),
+            fill_color=color_producer(el),
+            color="grey",
+            fill_opacity=0.7,
         )
     )
-# add the feature group to the map
-map.add_child(fg)
+
+fg_p = folium.FeatureGroup(name="Population")
+# add geojson object, which reads geo data from world.json file (the x in lambda represents features)
+fg_p.add_child(
+    folium.GeoJson(
+        data=(open("Webmap_datasources/world.json", "r", encoding="utf-8-sig").read()),
+        style_function=lambda x: {
+            "fillColor": "yellow"
+            if x["properties"]["POP2005"] < 10000000
+            else "orange"
+            if 10000000 <= x["properties"]["POP2005"] < 20000000
+            else "red"
+        },
+    )
+)
+# add the feature groups to the map
+map.add_child(fg_v)
+map.add_child(fg_p)
+# add layer control to the map (this allows control for feature groups)
+map.add_child(folium.LayerControl())
 # convert the map object to a html file
 map.save("Map1.html")
